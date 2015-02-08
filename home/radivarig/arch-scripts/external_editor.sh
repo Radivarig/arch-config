5caaeefede8bead42e760942d9a4f3f98585a76e
Radivarig <reslav.hollos@gmail.com>
2015-02-08 22:28:35 +0100

    improved monodevelop script

--- home/radivarig/arch-scripts/external_editor.sh
+++ home/radivarig/arch-scripts/external_editor.sh
@@ -1,6 +1,42 @@
 #!/bin/sh
 if [ -z "$1" ]; then exit 1; fi;
-#/bin/subl "`wine winepath -u "$1"`:$2:$3" #use this for opening in any other native text editor, but check how their command line arguments should be formatted!!
-export WINEPREFIX=~/.local/share/wineprefixes/unity3d/
-wine ~/.local/share/wineprefixes/unity3d/drive_c/Program\ Files/Unity/MonoDevelop/bin/monodevelop.exe "$1;$2"
+
+FILE_PATH=$(winepath -u "$1")
+
+if [[ $FILE_PATH == *".js" ]]
+then
+	export WINEPREFIX=~/.local/share/wineprefixes/unity3d/
+	wine ${WINEPREFIX}drive_c/Program\ Files/Unity/MonoDevelop/bin/MonoDevelop.exe "$1;$2"
+	exit 0
+fi
+
+FILE_LINE="$2"
+SLN_DIR="${FILE_PATH%%Assets/*}"
+SLN_PATH=$(find "$SLN_DIR" -maxdepth 1 -name "*.sln")
+SLN_NAME="${SLN_PATH#$SLN_DIR}"
+LOCAL_FILE_PATH="${FILE_PATH#$SLN_DIR}"
+LOCAL_FILE_DIR=$(dirname "$LOCAL_FILE_PATH")
+FILE_NAME="${LOCAL_FILE_PATH#$LOCAL_FILE_DIR}"
+FILE_NAME="${FILE_NAME#?}"
+
+#to go back ../ for number of folders in which file is -1
+COUNT="${LOCAL_FILE_PATH//[^\/]}"
+COUNT="${#COUNT}"
+COUNT="$((COUNT - 1))"
+
+BACKWARD_SLN_DIR = ""
+for i in $(seq 0 $COUNT); do BACKWARD_SLN_DIR="${BACKWARD_SLN_DIR}../"; done
+
+ln -s "/" "${SLN_DIR}/Z:"
+cd "${SLN_DIR}/$LOCAL_FILE_DIR"
+
+PREV_SLN_NAME=$(head -n 1 "${SLN_DIR}/sln_name_of_last_monodevelop_call")
+
+if [ "$(pidof monodevelop)" ] && [ $PREV_SLN_NAME == $SLN_NAME ]
+then /bin/monodevelop "$FILE_NAME;$FILE_LINE"
+else /bin/monodevelop "${BACKWARD_SLN_DIR}/$SLN_NAME $FILE_NAME;$FILE_LINE"
+fi
+
+echo "$SLN_NAME" > "${SLN_DIR}/sln_name_of_last_monodevelop_call"
+
 exit 0
