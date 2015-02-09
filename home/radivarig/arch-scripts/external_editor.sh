7931037ee5ef360270c6b6aa10f72e9c369d0721
Radivarig <reslav.hollos@gmail.com>
2015-02-09 15:58:37 +0100

    [fixed]double brackets

--- home/radivarig/arch-scripts/external_editor.sh
+++ home/radivarig/arch-scripts/external_editor.sh
@@ -2,7 +2,6 @@
 if [ -z "$1" ]; then exit 1; fi;
 
 FILE_PATH=$(winepath -u "$1")
-
 if [[ $FILE_PATH == *".js" ]]
 then
 	export WINEPREFIX=~/.local/share/wineprefixes/unity3d/
@@ -11,30 +10,29 @@ then
 fi
 
 FILE_LINE="$2"
-SLN_DIR="${FILE_PATH%%Assets/*}"
-SLN_PATH=$(find "$SLN_DIR" -maxdepth 1 -name "*.sln")
-SLN_NAME="${SLN_PATH#$SLN_DIR}"
+SLN_DIR="${FILE_PATH%%/Assets/*}"
+SLN_PATH=$(find "$SLN_DIR" -maxdepth 1 -name "*-csharp.sln")
+SLN_NAME="${SLN_PATH#${SLN_DIR}/}"
+SLN_NAME="${SLN_NAME#$?}"
 LOCAL_FILE_PATH="${FILE_PATH#$SLN_DIR}"
 LOCAL_FILE_DIR=$(dirname "$LOCAL_FILE_PATH")
-FILE_NAME="${LOCAL_FILE_PATH#$LOCAL_FILE_DIR}"
-FILE_NAME="${FILE_NAME#?}"
+FILE_NAME="${LOCAL_FILE_PATH#${LOCAL_FILE_DIR}/}"
 
-#to go back ../ for number of folders in which file is -1
 COUNT="${LOCAL_FILE_PATH//[^\/]}"
 COUNT="${#COUNT}"
-COUNT="$((COUNT - 1))"
+COUNT="$((COUNT - 2))"
 
 BACKWARD_SLN_DIR = ""
 for i in $(seq 0 $COUNT); do BACKWARD_SLN_DIR="${BACKWARD_SLN_DIR}../"; done
 
 ln -s "/" "${SLN_DIR}/Z:"
-cd "${SLN_DIR}/$LOCAL_FILE_DIR"
+cd "${SLN_DIR}$LOCAL_FILE_DIR"
 
 PREV_SLN_NAME=$(head -n 1 "${SLN_DIR}/sln_name_of_last_monodevelop_call")
 
 if [ "$(pidof monodevelop)" ] && [ $PREV_SLN_NAME == $SLN_NAME ]
 then /bin/monodevelop "$FILE_NAME;$FILE_LINE"
-else /bin/monodevelop "${BACKWARD_SLN_DIR}/$SLN_NAME $FILE_NAME;$FILE_LINE"
+else /bin/monodevelop "${BACKWARD_SLN_DIR}$SLN_NAME $FILE_NAME;$FILE_LINE"
 fi
 
 echo "$SLN_NAME" > "${SLN_DIR}/sln_name_of_last_monodevelop_call"
